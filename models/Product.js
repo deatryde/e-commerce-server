5d117cbf4a4bd4baca32ce34f6789285f819d90d
Rahul Dogra <rahuldogra1998@gmail.com>
2021-06-07 02:34:19 +0530

    Creating New product Model and APi

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const ProductScheme = new mongoose.Schema({
   userId: {
     type: Schema.Types.ObjectId,
--- routes/productsApi.js
+++ routes/productsApi.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/authorization");
+const { body, validationResult } = require("express-validator");
+const Product = require("../models/Product");
 
 router.get("/", (req, res) => {
   res.send("Products route");
 });
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      body("name", "Name is Required").not().isEmpty(),
+      body("description", "Description is Required").not().isEmpty(),
+      body("category", "Category is Required").not().isEmpty(),
+      body("price", "Price is Required").not().isEmpty(),
+      body("quantity", "Quantity is Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { name, description, category, price, quantity } = req.body;
+      const newProduct = new Product({
+        userId: req.user.id,
+        name,
+        description,
+        category,
+        price,
+        quantity,
+      });
+      const product = await newProduct.save();
+      res.json({ product });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
