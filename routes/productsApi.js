b60d58e47709079818006c5376d3510be66b8ab0
Rahul Dogra <rahuldogra1998@gmail.com>
2021-06-07 02:41:42 +0530

    Get all product and specific product api

--- routes/productsApi.js
+++ routes/productsApi.js
@@ -4,10 +4,32 @@ const auth = require("../middleware/authorization");
 const { body, validationResult } = require("express-validator");
 const Product = require("../models/Product");
 
-router.get("/", (req, res) => {
-  res.send("Products route");
+//Get all Products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
+//Getting Specific Product
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ msg: "Product was Not Found" });
+    }
+    res.json(product);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//Adding new Product
 router.post(
   "/",
   [
