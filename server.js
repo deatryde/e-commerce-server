eefafc893752b33f6163c55efe604789454635ef
Rahul Dogra <rahuldogra1998@gmail.com>
2021-06-09 02:02:45 +0530

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 8080;
 
+app.use(cors);
 //connect to mongoDB
 connectDB();
 
