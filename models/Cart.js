5155f9fef1b1257d3b9396101bd26ea390e9a5ef
Rahul Dogra <rahuldogra1998@gmail.com>
2021-07-29 01:45:47 +0530

    Cart Api and Schema debug

--- models/Cart.js
+++ models/Cart.js
@@ -11,7 +11,7 @@ const CartSchema = new Schema(
       required: true,
     },
     fulfilled: {
-      type: Booleon,
+      type: Boolean,
       default: false,
     },
   },
---
+++ routes/cartApi.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/authorization");
+const { isEmpty } = require("lodash");
+const Product = require("../models/Product");
+const Cart = require("../models/Cart");
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const carts = await Cart.find({ userId });
+    if (isEmpty(carts)) {
+      return res.send({ products: [] });
+    }
+    let retreiveCart;
+    carts.forEach((cart) => {
+      if (!cart.fulfilled) {
+        retreiveCart = cart;
+      }
+    });
+    let products = [];
+    let result;
+    if (!isEmpty(retreiveCart)) {
+      products = retreiveCart.products.map(
+        async (product) => await Product.findById({ _id: product })
+      );
+      products = await Promise.all(products);
+      result = { ...retreiveCart.toJSON(), products };
+    }
+    res.send({ result });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const cartId = req.params.cartId;
+    const product = req.body.product;
+    const cart = await Cart.updateOne(
+      { _id: cartId },
+      { $pullAll: { products: [product] } }
+    );
+    res.send({ cart });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.post("/", auth, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { products } = req.body;
+    let cart, unfulfilledCart;
+    const carts = await Cart.find({ userId });
+    const hasValidCarts = carts.reduce((acc, value) => {
+      if (!value.fulfilled) {
+        unfulfilledCart = value;
+      }
+      return acc && value.fulfilled;
+    }, true);
+    if (hasValidCarts) {
+      cart = new Cart({ userId, products });
+      cart = await cart.save();
+    } else {
+      const stringProduct = [...unfulfilledCart.products, ...products].map(
+        (product) => product.toString()
+      );
+      const newProducts = Array.from(new Set(stringProduct));
+      cart = await Cart.findByIdAndUpdate(
+        { _id: unfulfilledCart._id },
+        { products: newProducts }
+      );
+    }
+    let value = cart.products.map(async (id) => await Product.findById(id));
+    value = await Promise.all(value);
+    res.send({ ...cart.toJSON(), products: value });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
